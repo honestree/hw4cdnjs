0e5afda88c26f6aa154d1318a8f6861ce8834067
honestree <honestree.cs04@nctu.edu.tw>
2016-11-14 19:48:27 +0800

    @x09326 Practice version control using git

--- hw1.c
+++ hw1.c
@@ -2,29 +2,5 @@
 #include <stdlib.h>
 
 int main(){
-   int count = 0, ten, unit;
-   while( count < 100 ){
-   	ten = count / 10;
-	unit = count % 10;
-	if( ten * unit == 0 ){
-		if( ten == 0 ){
-			printf(" %d ", unit ); 
-		}
-		else
-			printf(" %d ", ten );
-	}
-	else{
-		if( ten * unit < 10 ){
-			printf(" %d ", unit * ten );
-		}
-		else{
-			printf("%d ", unit *ten );
-	
-		}
-	}
-	if( unit == 9 ){
-		printf("\n");
-	}
-   	++count;
-   }
+	printf("Hello World");
 }
